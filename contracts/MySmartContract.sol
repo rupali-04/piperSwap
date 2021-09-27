b0df4ffde3ced1210eff887ddb4b4a6a8148d8ec
unknown <rupalishah040@gmail.com>
2021-09-27 16:27:57 +0530

    Initial Commit

--- contracts/MySmartContract.sol
+++ contracts/MySmartContract.sol
@@ -1,16 +1,66 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import "./PiperToken.sol";
 
 contract MySmartContract {
-    IERC20 dai;
-    constructor(address _daiToken) {
+    address public owner;
+    IERC20 public dai;
+    PiperToken public pToken;
+
+    address[] public investers;
+    mapping(address => uint) public investingBalance;
+    mapping(address => bool) public hasInvested;
+    mapping(address => bool) public isInvesting;
+    constructor(address _daiToken, PiperToken _pToken) {
         dai = IERC20(_daiToken);
+	pToken = _pToken;
+	owner = msg.sender;
     }
 
-    function send() view public returns (uint) {
+    function bal() view public returns (uint) {
         return dai.balanceOf(address(msg.sender));
     }
+    
+    function investTokens(uint _amount) public {
+        // Require amount greater than 0
+        require(_amount > 0, "amount cannot be 0");
+
+        // Trasnfer Mock Dai tokens to this contract for investing
+        dai.transferFrom(msg.sender, address(this), _amount);
+	pToken.transfer(msg.sender, _amount);
+
+        // Update investing balance
+        investingBalance[msg.sender] = investingBalance[msg.sender] + _amount;
+
+        // Add user to investers array *only* if they haven't invested already
+        if(!hasInvested[msg.sender]) {
+            investers.push(msg.sender);
+        }
+
+        // Update investing status
+        isInvesting[msg.sender] = true;
+        hasInvested[msg.sender] = true;
+    }
+
+    function withdraw() public {
+        // Fetch staking balance
+        uint balance = investingBalance[msg.sender];
+
+        // Require amount greater than 0
+        require(balance > 0, "investing balance cannot be 0");
+
+        // Transfer Mock Dai tokens to this contract for staking
+        dai.transfer(msg.sender, balance);
+
+        // Reset investing balance
+        investingBalance[msg.sender] = 0;
+
+        // Update investing status
+        isInvesting[msg.sender] = false;
+    }
+	
+	
 }
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,11 @@
-//const Dai = artifacts.require("Dai");
+const PiperToken = artifacts.require("PiperToken");
 const MySmartContract = artifacts.require("MySmartContract");
 module.exports = async function (deployer,network,accounts) {
-    deployer.deploy(MySmartContract,'0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735');
+    deployer.deploy(PiperToken);
+    const PToken = await PiperToken.deployed();
+    deployer.deploy(MySmartContract,'0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735',PToken.address);
     await MySmartContract.deployed();
 
+    // Transfer all tokens to MySmartContract (1 million)
+    await PToken.transfer(MySmartContract.address, '1000000000000000000000000');
 };
