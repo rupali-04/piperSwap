596286394aebdbed248fcffe75947a991ff9efab
unknown <rupalishah040@gmail.com>
2021-10-01 10:43:48 +0530

    frontend modified

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,10 @@
 const PiperToken = artifacts.require("PiperToken");
+const DaiToken = artifacts.require("IERC20")
 const MySmartContract = artifacts.require("MySmartContract");
 module.exports = async function (deployer,network,accounts) {
     await deployer.deploy(PiperToken);
     const PToken = await PiperToken.deployed();
-    
+    const dToken = await DaiToken.at('0x4f96fe3b7a6cf9725f59353f723c1bdb64ca6aa');
     await deployer.deploy(MySmartContract,'0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735',PToken.address);
     await MySmartContract.deployed();
 
--- src/App.js
+++ src/App.js
@@ -1,12 +1,143 @@
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import DaiToken from '../src/build/contracts/IERC20.json'
+import PiperToken from '../src/build/contracts/PiperToken.json'
+import TokenFarm from '../src/build/contracts/MySmartContract.json'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
 
+class App extends Component {
+  
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
 
+  async loadBlockchainData() {
+    const web3 = window.web3
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Hello World!!</h1>
-    </div>
-  );
+    const accounts = 'await web3.eth.getAccounts()'
+    this.setState({ account: accounts[0] })
+
+    const networkId = 42
+    const daiAddress = '0x4f96fe3b7a6cf9725f59d353f723c1bdb64ca6aa';
+
+    // Load DaiToken
+    const daiTokenData = 42
+    if(daiTokenData) {
+      const daiToken = new web3.eth.Contract(DaiToken.abi, daiAddress)
+      this.setState({ daiToken })
+      let daiTokenBalance = await daiToken.methods.balanceOf(this.state.account).call()
+      this.setState({ daiTokenBalance: daiTokenBalance.toString() })
+    } else {
+      window.alert('DaiToken contract not deployed to detected network.')
+    }
+
+    // Load PiperToken
+    const piperTokenData = PiperToken.networks[networkId]
+    if(piperTokenData) {
+      const piperToken = new web3.eth.Contract(PiperToken.abi, piperTokenData.address)
+      this.setState({ piperToken })
+      let piperTokenBalance = await piperToken.methods.balanceOf(this.state.account).call()
+      this.setState({ piperTokenBalance: piperTokenBalance.toString() })
+    } else {
+      window.alert('PiperToken contract not deployed to detected network.')
+    }
+
+    // Load TokenFarm
+    const tokenFarmData = TokenFarm.networks[networkId]
+    if(tokenFarmData) {
+      const tokenFarm = new web3.eth.Contract(TokenFarm.abi, tokenFarmData.address)
+      this.setState({ tokenFarm })
+      let stakingBalance = await tokenFarm.methods.stakingBalance(this.state.account).call()
+      this.setState({ stakingBalance: stakingBalance.toString() })
+    } else {
+      window.alert('TokenFarm contract not deployed to detected network.')
+    }
+
+    this.setState({ loading: false })
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  stakeTokens = (amount) => {
+    this.setState({ loading: true })
+    this.state.daiToken.methods.approve(this.state.tokenFarm._address, amount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+      this.state.tokenFarm.methods.stakeTokens(amount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.setState({ loading: false })
+      })
+    })
+  }
+
+  unstakeTokens = (amount) => {
+    this.setState({ loading: true })
+    this.state.tokenFarm.methods.unstakeTokens().send({ from: this.state.account }).on('transactionHash', (hash) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '0x0',
+      daiToken: {},
+      piperToken: {},
+      tokenFarm: {},
+      daiTokenBalance: '0',
+      piperTokenBalance: '0',
+      stakingBalance: '0',
+      loading: true
+    }
+  }
+
+  render() {
+    let content
+    if(this.state.loading) {
+      content = <p id="loader" className="text-center">Loading...</p>
+    } else {
+      content = <Main
+        daiTokenBalance={this.state.daiTokenBalance}
+        piperTokenBalance={this.state.piperTokenBalance}
+        stakingBalance={this.state.stakingBalance}
+        stakeTokens={this.stakeTokens}
+        unstakeTokens={this.unstakeTokens}
+      />
+    }
+
+    return (
+      <div>
+        <Navbar account={this.state.account} />
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
+              <div className="content mr-auto ml-auto">
+                <a
+                  href="index.html"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                </a>
+
+                {content}
+
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
